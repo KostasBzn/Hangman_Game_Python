c47b01fcc407f3dd66bcfd700947c2000913addb
Kostas Bouzianis <kostasbouzianis@gmail.com>
2024-11-14 17:34:20 +0100

    finelizing - underscire replacement added

--- hangman.py
+++ hangman.py
@@ -3,30 +3,45 @@ import random
 def hangman():
     words = ["python", "chair", "programming", "berlin", "laptop", "bootcamp", "winter"]
     random_word_to_guess = random.choice(words)
+    word_to_guess = "_"*len(random_word_to_guess)
     guessed_letters = []
     attempts = 6
     print("Welcome to Hangman!")
-    print("Try to guess the word..you have 6 wrong attempts!")
+    print("Try to guess the word..you have 6 wrong attempts!\n")
     while attempts > 0:
+       print("Progress: ", word_to_guess)
+       print(f"You have {attempts} attempts left.")
        guessed_letter = input("Please guess a letter:").lower()
        
        if not guessed_letter.isalpha() or len(guessed_letter) != 1:
-            print("Please type a valid alphabetic character.")
+            print("Please type a valid alphabetic character.\n")
             continue
        
        if guessed_letter in guessed_letters:
-           print(f"You have already guessed the letter {guessed_letter}")
+           print(f"You have already guessed the letter {guessed_letter}\n")
            continue
        
        guessed_letters.append(guessed_letter)
-
-       if guessed_letter not in random_word_to_guess:
-           attempts -= 1
-           print(f"Sorry, the letter {guessed_letter} is not in the word")
-           print(f"You have {attempts} attempts left.")
        
        if guessed_letter in random_word_to_guess:
-           print(f"Good job! The letter {guessed_letter} is in the word")
+            print(f"Good job! The letter {guessed_letter} is in the word\n")
+
+            word_to_guess_list = list(word_to_guess)
+
+            for i in range(0, len(random_word_to_guess)):
+              if random_word_to_guess[i] == guessed_letter:
+                word_to_guess_list[i] = guessed_letter  # Update guessed letter
+
+        # Convert list back to string
+            word_to_guess = ''.join(word_to_guess_list)
+            
+            if "_" not in word_to_guess:
+                print(f"Congratulations! You won! The word is {random_word_to_guess}")
+                break
+       else:
+            attempts -= 1
+            print(f"Sorry, the letter {guessed_letter} is not in the word\n")
+            
 
        if attempts == 0:
            print(f"Sorry you have run out of attempts :(")
@@ -35,10 +50,5 @@ def hangman():
            if restart == "exit":
                print("That was fun! Until next time!")
                break
-       
-
-       
-       
-
-
+           
 hangman()
